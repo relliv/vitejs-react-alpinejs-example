e6009e0a8532105201ca674039f93b5fe5e6ac76
relliv <epsilonelittr@gmail.com>
2024-06-28 01:37:24 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+Alpine.data('appData', () => ({
+  // state of mobile hamburger menu
+  count: 0,
+
+  increaseCount() {
+    this.count += 1;
+  },
+}));
